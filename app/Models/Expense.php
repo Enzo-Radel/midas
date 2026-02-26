39fa0e7edf54abd6e7d1395054cc8cc90ac38624
Enzo Radel <enzo@radel.com.br>
2026-02-26 00:49:07 -0300

    cria a tabela e a model de expenses

---
+++ app/Models/Expense.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'amount',
+        'description',
+        'date',
+        'user_id',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'date' => 'date',
+        ];
+    }
+
+    /**
+     * Get the user that owns the expense.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
